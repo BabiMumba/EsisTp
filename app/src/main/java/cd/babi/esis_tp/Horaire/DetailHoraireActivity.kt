8b1fe26a945b67cfd9014c4a1dea7467640f8730
Babi Mumba <babimumba243@gmail.com>
2023-09-18 21:53:50 +0200

    ajouter de filiere manuellement puis creation d'une activite detailhoraire

---
+++ app/src/main/java/cd/babi/esis_tp/Horaire/DetailHoraireActivity.kt
@@ -0,0 +1,12 @@
+package cd.babi.esis_tp.Horaire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cd.babi.esis_tp.R
+
+class DetailHoraireActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_horaire)
+    }
+}
--- app/src/main/java/cd/babi/esis_tp/Horaire/HoraireFragment.kt
+++ app/src/main/java/cd/babi/esis_tp/Horaire/HoraireFragment.kt
@@ -20,7 +20,23 @@ class HoraireFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_horaire, container, false)
 
         // Création de la liste de filières
-        val filieres = createFiliereList()
+        //val filieres = createFiliereList()
+        val filieres = mutableListOf<PromModel>(
+            PromModel("L1", "L1"),
+            PromModel("L2", "L2"),
+            PromModel("L3_Gl", "L3"),
+            PromModel("L3_MSI", "M1"),
+            PromModel("L3_AS", "M2"),
+            PromModel("L3_DESIGN", "M2"),
+            PromModel("L3_TLC", "M2"),
+            PromModel("L4_Gl", "L3"),
+            PromModel("L4_MSI", "M1"),
+            PromModel("L4_AS", "M2"),
+            PromModel("L4_DESIGN", "M2"),
+            PromModel("L4_TLC", "M2"),
+        )
+
+        recyclerView = view.findViewById(R.id.recy_horaire)
 
         adapter = PromAdapter(filieres)
         recyclerView.adapter = adapter
